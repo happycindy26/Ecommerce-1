368a2b0de37e157a22118010045660eec16fb6be
Cindy <happycindy26@gmail.com>
2021-03-04 22:23:53 +1100

    updated home image

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,25 +1,27 @@
 import React, {Component} from 'react';
 import {ReactPlayer} from 'react-player';
 import video from '../../images/Botani_Insta.mp4';
+import './About.css';
 
 class About extends Component {
     render() {
         return (
-            <div>
-                
-                <div>
-                <iframe className='video mx-5 my-5'
+            <div className="About">
+                <h3 className="text-center mt-5">Organic, Vegan Skincare Products Australia</h3>
+                <div className="intro">
+                <iframe className='video mx-5'
                     width=""
-                    height=""
+                    height="300"
                         src={video}
                     frameborder="0">
                 </iframe>
-                </div>
-                <div className="mx-5 my-4">
+                
+                <div className="mx-5 mt-5">
                     <p>100% botanically based natural problem-solving skincare and pharmaceutical products. Our skincare products contain a blend of high quality certified organic ingredients and skin-friendly plant actives that are in sync with your skin.</p>
                     <p>At Botani, we only use natural plant-based ingredients. Each product is formulated using olive derived plant actives, including our signature ingredient Olive Squalene, traditional herbal extracts, potent superfoods and Australian native plant extracts. Our products are vegan certified and cruelty-free.</p>
+                    <p>100% botanically based natural problem-solving skincare 100% botanically based natural problem-solving skincare and pharmaceutical products. Our skincare products contain a blend of high quality certified organic ingredients and skin-friendly plant actives that are in sync with your skin.</p>
                 </div>
-                
+                </div>
             </div>  
             
         )
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
-import groupProducts from '../../images/blond.jpg';
+import './Home.css';
+import homeImg from '../../images/pexels.jpg';
+
 class Home extends Component {
     render() {
         return (
-            <div class="card text-black text-center">
-                <img src={groupProducts} class="card-img" alt="..."/>
-                <div class="card-img-overlay mt-3">
-                    <h3 className="  ">Organic, Vegan Skincare Products Australia</h3>
+            <div class="card text-black text-left">
+                <img src={homeImg} class="card-img" alt="skin care"/>
+                <div class="card-img-overlay ml-5 mt-5">
+                    <h3 className="">Organic, Vegan Skincare Products Australia</h3>
                     <NavLink to="/botani/products">
                         <button type="button" className="btn btn-success my-4">SHOP NOW</button>
                     </NavLink>
