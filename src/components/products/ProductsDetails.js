2c89f5593ba1c31d0237b77ee4b42557edaac8f5
Cindy <happycindy26@gmail.com>
2021-03-14 23:34:33 +1100

    updated productsDetails page

--- src/components/products/ProductsDetails.js
+++ src/components/products/ProductsDetails.js
@@ -10,16 +10,16 @@ class ProductsDetails extends Component {
         const currentProduct = getProduct(name);
         return (
             <div className="container">
-                <div className="ProductsDetails row align-items-center ">
-                    <div className="col-6">
+                <div className="ProductsDetails row justify-content-center align-items-center ">
+                    <div className="col-11 col-lg-6">
                         <img src={currentProduct.src} alt={currentProduct.name}/> 
                         </div>
-                        <div className="col-3 text-center">
+                        <div className="col-6 text-center">
                             <h4 >{currentProduct.name}</h4>
                             <h4 >${currentProduct.price}</h4>    
                             <input type="number" max="20" min="0" />
                             <div class="w-100"></div>
-                            <Link to={`./${currentProduct.name}/addtocart`} className="btn btn-primary">
+                            <Link to={`./${currentProduct.name}/addtocart`} className="btn btn-primary my-5">
                                 ADD TO CART    
                             </Link>  
                         </div>  
