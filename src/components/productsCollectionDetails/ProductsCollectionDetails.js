85d40159c8736ad6945e8ecbe84b70e2fccae68e
Cindy <happycindy26@gmail.com>
2021-07-02 07:52:02 +1000

    create productsCollectionDetails component

---
+++ src/components/productsCollectionDetails/ProductsCollectionDetails.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {addItem} from '../../redux/cart/cart.actions';
+import './ProductsCollectionDetails.css';
+
+const ProductsCollectionDetails = ({currentProduct, addItem}) => (
+    <div className="container">
+                <div className="ProductsDetails row justify-content-center align-items-center ">
+                    <div className="col-11 col-lg-6">
+                        <img src={currentProduct.src} alt={currentProduct.name}/> 
+                        </div>
+                        <div className="col-6 text-center">
+                            <h4 >{currentProduct.name}</h4>
+                            <h4 >${currentProduct.price}</h4>  
+                            <div class="w-100"></div>
+                            <button onClick={() => addItem(currentProduct)} className="btn btn-outline-dark my-5">ADD TO CART </button>  
+                        </div>  
+                    </div>
+                <div className="row">
+                    <div className="mx-3 mb-5">
+                        <h4>Deccription:</h4>
+                        <p>{currentProduct.description}</p>
+                        <h4>Ingredients:</h4>
+                        <p>{currentProduct.ingredients}</p>
+                    </div>
+                </div> 
+            </div>
+)
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+export default connect(null, mapDispatchToProps)(ProductsCollectionDetails);
--- src/pages/productsDetails/ProductsDetails.js
+++ src/pages/productsDetails/ProductsDetails.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
-import {connect} from 'react-redux';
-import {addItem} from '../../redux/cart/cart.actions';
+import ProductsCollectionDetails from '../../components/productsCollectionDetails/ProductsCollectionDetails';
 import ProductsData from '../products/ProductsData';
 //import './ProductsDetails.css';
 
@@ -19,37 +18,14 @@ class ProductsDetails extends Component {
         getProduct(name);
   
         return (
-            <div className="container">
-                <div className="ProductsDetails row justify-content-center align-items-center ">
-                    <div className="col-11 col-lg-6">
-                        <img src={currentProduct.src} alt={currentProduct.name}/> 
-                        </div>
-                        <div className="col-6 text-center">
-                            <h4 >{currentProduct.name}</h4>
-                            <h4 >${currentProduct.price}</h4>  
-                            <input type="number" max="20" min="1" />
-                            <div class="w-100"></div>
-                            <button onClick={() => addItem(currentProduct)} className="btn btn-outline-dark my-5">ADD TO CART </button>  
-                        </div>  
-                    </div>
-                <div className="row">
-                    <div className="mx-3 mb-5">
-                        <h4>Deccription:</h4>
-                        <p>{currentProduct.description}</p>
-                        <h4>Ingredients:</h4>
-                        <p>{currentProduct.ingredients}</p>
-                    </div>
-                </div> 
-            </div>
+            <ProductsCollectionDetails currentProduct = {currentProduct} />
         )
     }
 }
 
-const mapDispatchToProps = dispatch => ({
-    addItem: item => dispatch(addItem(item))
-})
 
-export default connect(null, mapDispatchToProps)(ProductsDetails);
+
+export default ProductsDetails;
 
 /*
 <Link to={`./${currentProduct.name}/addtocart`} className="btn btn-outline-dark my-5">
